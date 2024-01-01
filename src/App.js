2e20b4070f174da8bfb06814bd9f0cae5d9e5ad6
KashishKataria <141554217+KashishKataria@users.noreply.github.com>
2023-12-31 20:19:59 -0800

    hgh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Route, Routes} from "react-router-dom";
+import HeroSection from "./components/HeroSection";
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Hero></Hero>
-      <Footer></Footer>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <Header/>
+                <Routes>
+                    <Route path={"/"} element={<HeroSection />}></Route>
+                </Routes>
+            <Footer/>
+        </div>
+    );
 }
-
-export default App;
